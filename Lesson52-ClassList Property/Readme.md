9a161ed7656eb7c9c14a4bf02f6fa5f98176f802
nhan10cdt1 <nhanbkdn1211@gmail.com>
2024-01-06 20:59:39 +0900

    javascript basic

---
+++ Lesson52-ClassList Property/Readme.md
@@ -0,0 +1,17 @@
+## Class list property
+
+1. add
+
+   - Thêm class vào element
+
+2. contains
+
+   - Kiểm tra xem đả tồn tại class hay chưa
+
+3. remove
+
+   - xóa class khỏi element
+
+4. toggle
+
+   - Có thì gỡ bỏ , chưa có thì thêm
---
+++ Lesson52-ClassList Property/practice01.js
@@ -0,0 +1,15 @@
+var boxElement = document.querySelector(".box");
+//add
+boxElement.classList.add("class-1", "class-2");
+
+//contains
+
+console.log(boxElement.classList.contains("class-1"));
+// remove
+boxElement.classList.remove("class-1");
+
+// toggle
+boxElement.classList.toggle("red");
+
+// element
+console.log(boxElement);
